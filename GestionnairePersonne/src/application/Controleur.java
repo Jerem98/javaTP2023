9239a8b34ab8e8edd4d6835e82018cab8efe98c4
Jerem98 <57532244+Jerem98@users.noreply.github.com>
2023-07-04 12:04:06 +0200

    Add files via upload
    Second Envoi

--- GestionnairePersonne/src/application/Controleur.java
+++ GestionnairePersonne/src/application/Controleur.java
@@ -2,6 +2,16 @@
 
 package application;
 
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class Controleur {
+	TextField tfNom = new TextField();
+	TextField tfAnnee = new TextField();
+	TextField tfCategorie = new TextField();
+	ListView lvPersonnes = new ListView();
 	
+	void initialize(){
+		
+	}
 }
